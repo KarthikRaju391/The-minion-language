d1381c0f803aa745e653da5ed0b558805ad05e2c
Karthik Raju <karthikraju391@gmail.com>
2021-04-11 19:16:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var btnTranslate = document.querySelector('#btn-translate');
+var textInput = document.querySelector('#text-enter');
+var outputDiv = document.querySelector('#output');
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslation(inText) {
+	return serverURL + '?' + 'text=' + inText;
+}
+
+function errorHandler(error) {
+	console.log('Error occured', error);
+	alert('Something is wrong with the server, try again after sometime');
+}
+
+function clickEventHandler() {
+	var inputText = textInput.value; //taking input
+
+	fetch(getTranslation(inputText))
+		.then((response) => response.json())
+		.then((json) =>{
+            var translatedText = json.contents.translated;
+           outputDiv.innerText = translatedText; //output
+        })
+		.catch(errorHandler)
+}
+btnTranslate.addEventListener('click', clickEventHandler);
